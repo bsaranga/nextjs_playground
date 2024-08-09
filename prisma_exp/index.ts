ff9b9449db4096cfc9778022cacbe3e9183f4898
Buwaneka Saranga <bsaranga@gmail.com>
2024-08-09 10:56:27 +0530

    testing out prisma

---
+++ prisma_exp/index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.user.create({
+        data: {
+            name: "Fred",
+            email: "fred@prisma.io",
+            posts: {
+                create: { title: "Hello Venus" },
+            },
+            profile: {
+                create: { bio: "I like virgins" },
+            },
+        }
+    });
+
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+            profile: true,
+        },
+    });
+
+    console.dir(allUsers, { depth: null });
+}
+
+main().then(async () => {
+    await prisma.$disconnect();
+}).catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+})
